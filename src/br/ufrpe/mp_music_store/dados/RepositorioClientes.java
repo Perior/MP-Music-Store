ca266cf59137e8b818d8739508d01ae9bf836699
Matheus <mthcavalcanti@gmail.com>
2017-11-20 22:00:41 -0300

    Repositorios cd, cliente e funcionario

--- src/br/ufrpe/mp_music_store/dados/RepositorioClientes.java
+++ src/br/ufrpe/mp_music_store/dados/RepositorioClientes.java
@@ -1,5 +1,87 @@
 package br.ufrpe.mp_music_store.dados;
+import br.ufrpe.mp_music_store.negocio.beans.Cliente;
 
 public class RepositorioClientes {
-
+	
+	private Cliente[] cliente;
+	private int proxima;
+	
+	//Construtor
+	public RepositorioClientes(int tamanho){
+		this.cliente = new Cliente[tamanho];
+		this.proxima = 0;
+	}
+	
+	//Cadastrar Cliente
+	public void cadastrar(Cliente c){
+		this.cliente[this.proxima] = c;
+		this.proxima += 1;
+		if(this.proxima == cliente.length){
+			this.duplicarArrayClientes();
+		}
+	}
+	
+	public void cadastrar(String nome, int cpf, String endereco, int tel, int numCadastro){
+		Cliente cl = new Cliente(nome, cpf, endereco, tel, numCadastro);
+		this.cadastrar(cl);
+	}
+	
+	//Buscar cliente por CPF
+	public Cliente buscar(int cpf){
+		int i = this.procurarIndice(cpf);
+		Cliente resultado = null;
+		if(i != this.proxima){
+			resultado = this.cliente[i];
+		}
+		return resultado;
+	}
+	
+	//Buscar índice
+	private int procurarIndice(int cpf){
+		int i = 0;
+		boolean achou = false;
+		
+		while((!achou) && (i < this.proxima)){
+			if(cpf == this.cliente[i].getCpf()){
+				achou = true;
+			}else{
+				i += 1;
+			}
+		}
+		return i;
+	}
+	
+	//Verficar se cliente existe
+	public boolean existe(int cpf){
+		boolean existe = false;
+		int indice = this.procurarIndice(cpf);
+		if(indice != proxima){
+			existe = true;
+		}
+		return existe;
+	}
+	
+	//Remover do array de clientes
+	public void remover(int cpf){
+		int i = this.procurarIndice(cpf);
+		if(i != this.proxima){
+			this.cliente[i] = this.cliente[this.proxima - 1];
+			this.cliente[this.proxima] = null;
+			this.proxima = this.proxima - 1;
+		}else{
+			
+		}
+	}
+	
+	//Dobrar tamanho do array de clientes
+	public void duplicarArrayClientes(){
+		if(this.cliente != null && this.cliente.length > 0){
+			Cliente arrayDuplicado[] = new Cliente[this.cliente.length * 2];
+			
+			for(int i = 0; i < this.cliente.length; i++){
+				arrayDuplicado[i] = this.cliente[i];
+			}	
+			this.cliente = arrayDuplicado;
+		}	
+	}
 }
--- src/br/ufrpe/mp_music_store/dados/RepositorioFuncionarios.java
+++ src/br/ufrpe/mp_music_store/dados/RepositorioFuncionarios.java
@@ -1,5 +1,91 @@
 package br.ufrpe.mp_music_store.dados;
+import br.ufrpe.mp_music_store.negocio.beans.Funcionario;
 
 public class RepositorioFuncionarios {
-
+	
+	private Funcionario[] funcionario;
+	private int proxima;
+	
+	//Construtor
+	public RepositorioFuncionarios(int tamanho){
+		this.funcionario = new Funcionario[tamanho];
+		this.proxima = 0;
+	}
+	
+	//Cadastrar Funcionário
+	public void cadastrar(Funcionario f){
+		this.funcionario[this.proxima] = f;
+		this.proxima += 1;
+		if(this.proxima == funcionario.length){
+			this.duplicarArrayFuncionarios();
+		}
+	}
+	
+	public void cadastrar(String nome, int cpf, String endereco, int tel, float salario, 
+			int numContrato){
+		Funcionario f = new Funcionario(nome, cpf, endereco, tel, salario, numContrato);
+		this.cadastrar(f);
+	}
+	
+	//Buscar funcionário por cpf
+	public Funcionario buscar(int cpf){
+		int i = this.procurarIndice(cpf);
+		Funcionario resultado = null;
+		if(i != this.proxima){
+			resultado = this.funcionario[i];
+		}
+		
+		return resultado;
+	}
+	
+	//Buscar índice
+	public int procurarIndice(int cpf){
+		int i = 0;
+		boolean achou = false;
+		
+		while((!achou) && (i < this.proxima)){
+			if(cpf == this.funcionario[i].getCpf()){
+				achou = true;
+			}else{
+				i += 1;
+			}
+		}
+		return i;
+	}
+	
+	//Verificar se funcionário existe
+	public boolean existe(int cpf){
+		boolean existe = false;
+		int indice = this.procurarIndice(cpf);
+		if(indice != proxima){
+			existe = true;
+		}
+		
+		return existe;
+	}
+	
+	//Remover do array de funcionários
+	public void remover(int cpf){
+		int i = this.procurarIndice(cpf);
+		if(i != this.proxima){
+			this.funcionario[i] = this.funcionario[this.proxima - 1];
+			this.funcionario[this.proxima] = null;
+			this.proxima = this.proxima - 1;
+		}else{
+			
+		}
+	}
+	
+	//Duplicar tamanho do array de funcionários
+	public void duplicarArrayFuncionarios(){
+		if(this.funcionario != null && this.funcionario.length > 0){
+			Funcionario arrayDuplicado[] = new Funcionario[this.funcionario.length * 2];
+			
+			for(int i = 0; i < this.funcionario.length; i++){
+				arrayDuplicado[i] = this.funcionario[i];
+			}
+			
+			this.funcionario = arrayDuplicado;
+		}
+	}	
 }
